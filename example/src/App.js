bf835afb12838279a456e9eb4fe3fb8b9695cb31
MICHAel <plasteefff@gmail.com>
2019-03-26 21:16:23 +0300

    Fixed

--- example/src/App.js
+++ example/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 
-// import ExampleComponent from "code-artel-ui-lib";
+import ExampleComponent from "code-artel-ui-lib";
 
 export default class App extends Component {
   render() {
-    return <div>1</div>;
+    return (
+      <div>
+        <ExampleComponent />
+      </div>
+    );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,31 @@
 import * as React from "react";
+// import { Field, FieldRenderProps } from "react-final-form";
 
-export type Props = {};
+import Box from "./components/Box/Box";
+import ButtonBase from "./components/ButtonBase/ButtonBase";
+import Flex from "./components/Flex/Flex";
+import Input from "./components/Input/Input";
+// import CheckboxBase from "./components/CheckboxBase/CheckboxBase";
+
+// import TextArea from "./components/TextArea/TextArea";
+// import SelectBase from "./components/SelectBase/SelectBase";
+// import RadioCheckboxBase from "./components/RadioCheckboxBase/RadioCheckboxBase";
+
+export interface Props {
+  size?: string;
+  variant?: string;
+}
 
 export class ExampleComponent extends React.Component<Props> {
   render() {
-    return <div>1</div>;
+    return (
+      <div>
+        <Box>Box</Box>
+        <ButtonBase size={"large"}>Button</ButtonBase>
+        <Flex>Flex</Flex>
+        <Input variant={"default"} />
+      </div>
+    );
   }
 }
 
