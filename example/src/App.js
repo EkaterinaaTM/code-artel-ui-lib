8853afe8b97eee7a8bc810cbcb8814f8a7bbb3b7
MICHAel <plasteefff@gmail.com>
2019-03-27 15:42:20 +0300

    fixed bugs for import

--- example/src/App.js
+++ example/src/App.js
@@ -1,21 +1,20 @@
-import {
-  Box,
-  ButtonBase,
-  Flex,
-  Input,
-  CheckboxBase,
-  TextArea,
-  SelectBase,
-  RadioCheckboxBase
-} from "code-artel-ui-lib";
+import * as React from "react";
+import index$4 from "code-artel-ui-lib";
 
-export default {
-  Box,
-  ButtonBase,
-  Flex,
-  Input,
-  CheckboxBase,
-  TextArea,
-  SelectBase,
-  RadioCheckboxBase
+export const App = () => {
+  const { Box, ButtonBase, Flex, Input, CheckboxBase } = index$4;
+  console.log(index$4);
+  return (
+    <div>
+      <Box>Box</Box>
+      <ButtonBase variant={"default"} size={"medium"}>
+        ButtonBase
+      </ButtonBase>
+      <Flex>Flex</Flex>
+      <Input placeholder={"MICHA"} />
+      <CheckboxBase />
+    </div>
+  );
 };
+
+export default App;
