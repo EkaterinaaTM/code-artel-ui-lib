19799afa75d3f5a450ccb7621b6f9b37e3604ca0
fmakareev <fmakareev@code-artel.com>
2019-04-22 17:05:36 +0300

    new puild

--- src/styles/variants/Tooltip/TooltipVariant.tsx
+++ src/styles/variants/Tooltip/TooltipVariant.tsx
@@ -3,7 +3,9 @@ import {ITheme} from "../../interfaces";
 // ts-ignore
 export const TooltipVariant = (theme: ITheme): any => {
   return {
-    dark: {},
+    dark: {
+      color: theme.colors && theme.colors.dark,
+    },
     success: {},
     warning: { },
     error: { },
