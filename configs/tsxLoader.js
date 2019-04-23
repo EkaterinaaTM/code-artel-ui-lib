041db3085f46b51068857576aad6092dd3ea9d2a
fmakareev <fmakareev@code-artel.com>
2019-04-23 23:31:10 +0300

    Remove dead code

--- configs/tsxLoader.js
+++ configs/tsxLoader.js
@@ -1,19 +1,3 @@
-// const path = require('path');
-//
-// module.exports = {
-//   test: /\.(ts|tsx)$/,
-//   exclude: /node_modules/,
-//   use: [
-//     {
-//       loader: require.resolve('awesome-typescript-loader'),
-//       options:{
-//         configFileName: path.resolve(__dirname, "../tsconfig.json")
-//       }
-//     }
-//   ]
-// }
-
-
 module.exports = {
   test: /\.(ts|tsx)$/,
   exclude: /node_modules/,
